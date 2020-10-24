e77caf5f7c14552e5d8670e0cd1d5e5b7f3afbb2
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2020-10-23 23:08:48 -0300

    changes in the problem

--- src/database/schemas/problem.schema.ts
+++ src/database/schemas/problem.schema.ts
@@ -12,8 +12,17 @@ export class Problem extends Document {
     @Prop({ type: String, required: true })
     public image: string;
 
+    @Prop({ type: String, required: true })
+    public title: string;
+
+    @Prop({ type: String, required: true })
+    public description: string;
+
     @Prop({ type: LocationSchema, required: true })
     public location: Location;
+
+    @Prop({ type: String, required: true })
+    public status: string;
 
     @Prop({ type: [CommentSchema], default: [] })
     public comments: Comment[];
--- src/interfaces/problem.interface.ts
+++ src/interfaces/problem.interface.ts
@@ -1,8 +1,10 @@
 import { Document } from 'mongoose';
 import { Location } from './location.interface';
+import { Comment } from './comment.interface';
 
 export interface Problem extends Document {
     owner: string;
     image: string;
     location: Location;
+    comments: Comment[];
 }
